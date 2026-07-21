ff7f3a68810951a1ebd507f35d6968de5bf91dba
mvdgp <mvandraanen@hotmail.com>
2026-07-21 22:54:54 +0200

    fixed breaking images for projects due to location change

--- src/data.ts
+++ src/data.ts
@@ -1,8 +1,18 @@
+import donnybapImage from './assets/Project-Donnybap-large.png';
+import fietsmaatjesImage from './assets/Project-Fietsmaatjes-large.png';
 import portfolioData from './portfolioData.json';
 import { Project, SkillCategory, TimelineEvent } from './types';
 
+const projectImages: Record<string, string> = {
+	'Project-Donnybap-large.png': donnybapImage,
+	'Project-Fietsmaatjes-large.png': fietsmaatjesImage,
+};
+
 export const personalInfo = portfolioData.personalInfo;
-export const projectsData: Project[] = portfolioData.projects;
+export const projectsData: Project[] = portfolioData.projects.map((project) => ({
+	...project,
+	image: projectImages[project.image] ?? project.image,
+}));
 export const skillsData: SkillCategory[] = portfolioData.skills;
 export const timelineEvents: TimelineEvent[] = portfolioData.experience;
 export const education = portfolioData.education;
